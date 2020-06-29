d5db57de5bb56ba9c3524fcdd70ef40edf33f765
Firoze Sethna <62418404+XtremePwnership@users.noreply.github.com>
2020-06-29 11:48:18 +0400

    Add files via upload

---
+++ creds.py
@@ -0,0 +1,4 @@
+
+bot_token = "xxxxxxxxxxxxx" #MODIFYYYYTHIS #SENSITIZE
+bot_user_name = "xxxxxxxxx" #MODIFYYYYTHIS #SENSITIZE
+URL = "xxxxxxxxxxxx" #MODIFYYYYTHIS #SENSITIZE to debug locally run app.py & ngrok.exe & get URL from latter. Then just paste the ngrok URL here, save, then in browser head to ngrokURL/setwebhook and webhook will be set. Then you're good to roll. While testing locally be mindufl of UTC time adjustment in the code, don't bother changing the code, just input from user side mindfully.
